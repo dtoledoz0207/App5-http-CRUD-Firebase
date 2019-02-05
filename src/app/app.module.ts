756acab08987aecc720a699ee5fc7a1378327f72
David Toledo <dtoledoz0207@gmail.com>
2019-02-05 13:43:04 -0600

    heroesService-created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
@@ -8,6 +9,9 @@ import { HeroeComponent } from './components/heroes/heroe.component';
 
 // Rutas
 import {AppRoutingModule} from './app.routes';
+
+// Servicios 
+import {HeroesService} from './services/heroes.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,12 @@ import {AppRoutingModule} from './app.routes';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
